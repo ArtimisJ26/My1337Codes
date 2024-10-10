bbe864188d6437e8f3a14b8f37aaa5732c71507e
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2024-10-09 17:29:03 -0700

    Time: 481 ms (22.72%) | Memory: 35.3 MB (13.46%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,37 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        part = []
+        res = []  # all possible partitions
+        part = [] # current partition
 
+        # lets trast by partitioning it from the left side
         def dfs(left):
+            # base case, no more characters to partition
             if left >= len(s):
+                # part.copy() since "part" will change in different instances of the recursion
                 res.append(part.copy())     
                 return
+            # create possibilities of partitions from the left 1chars, 2chars, 3chars
             for i in range(left,len(s)):
+                # lets see if the left partition that we created is a palindrome
                 if self.isPalindrome(s, left, i):
+                    # add it to the partition array
                     part.append(s[left:i+1])
+                    # if yes, only then explore its further partitions
                     dfs(i+1)
-                    part.pop() 
-        # res = []  # all possible partitions
-        # part = [] # current partition
-
-        # # lets trast by partitioning it from the left side
-        # def dfs(left):
-        #     # base case, no more characters to partition
-        #     if left >= len(s):
-        #         # part.copy() since "part" will change in different instances of the recursion
-        #         res.append(part.copy())     
-        #         return
-        #     # create possibilities of partitions from the left 1chars, 2chars, 3chars
-        #     for i in range(left,len(s)):
-        #         # lets see if the left partition that we created is a palindrome
-        #         if self.isPalindrome(s, left, i):
-        #             # add it to the partition array
-        #             part.append(s[left:i+1])
-        #             # if yes, only then explore its further partitions
-        #             dfs(i+1)
-        #             # Remove the string that we just added above so that this variable can be reused in other iterations
-        #             part.pop()    
+                    # Remove the string that we just added above so that this variable can be reused in other iterations
+                    part.pop()    
                     
         dfs(0)
         return res
