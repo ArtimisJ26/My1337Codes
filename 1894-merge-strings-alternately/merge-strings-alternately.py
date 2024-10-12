397c4111d27a7af662f319765dd535c3ca10e7ca
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2024-10-12 16:07:19 -0700

    Time: 34 ms (65.19%) | Memory: 16.6 MB (39.97%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -3,20 +3,13 @@ class Solution:
         res = ""
         p1 = 0
         p2 = 0
-        counter = 0
 
-        while p1 < len(word1) and p2 < len(word2):
-            if counter % 2 == 0:
+        while p1 < len(word1) or p2 < len(word2):
+            if p1 < len(word1):
                 res += word1[p1]
                 p1 += 1
-            else:
+            if p2 < len(word2):
                 res += word2[p2]
                 p2 += 1
-            counter += 1
-        
-        if p2 <= len(word2)-1:
-            res += word2[p2:]
-        elif p1 <= len(word1)-1:
-            res += word1[p1:]
-        
+
         return res
