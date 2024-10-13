8b77fb5f1748379615ca088d1557fe4710161dc4
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2024-10-12 17:23:50 -0700

    Time: 731 ms (99.62%) | Memory: 30.8 MB (15.22%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
@@ -1,34 +1,15 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        maxSubset = 1
-        # [1,3,4,5,6,7,9]
-        # -> [x, x2, x4,]
-        # sort, check if its duplicate exists, if yes, then check if its square exists(recursively)
-        # [5,4,1,2,2]
-        arr = set(sorted(nums))
-        print(arr)
-        # [1,2,2,4,5]
-        def findSubset(i, count):
-            print(i,count)
-            if i not in arr:
-                return count-1
-            if nums.count(i) < 2:
-                return count+1
-            
-            if i != 1:
-                print("->",i,count)
-                count = findSubset(i**2, count+2)
-            return count
+        counts = Counter(nums)
+        dp = {}
+        res = max(1, counts[1] - 1 + counts[1]%2)
+        arr = sorted([k for k,v in counts.items() if v >= 2 and k > 1], reverse=True)
 
-        for i in arr:
-            if i < 10**5:
-                if i == 1:
-                    count = nums.count(i)
-                    if count % 2 == 0:
-                        count -= 1
-                else:
-                    count = findSubset(i,0)
-                # print(count)
-                maxSubset = max(maxSubset,count)
-
-        return maxSubset
+        for num in arr:
+            if num*num in dp:
+                dp[num] = dp[num*num] + 2
+                res = max(res, dp[num])
+            elif num*num in counts:
+                dp[num] = 3
+                res = max(res, dp[num])
+        return res
