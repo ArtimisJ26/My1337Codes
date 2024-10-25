a229aab67a084a39ef4bdf3f02a020981069759c
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2024-10-24 20:15:33 -0700

    Time: 951 ms (30.09%) | Memory: 36.9 MB (14.21%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -16,3 +16,22 @@ class Solution:
             return minCoins if minCoins != float('inf') else -1
                 
         return exploreCombinations(amount)
+
+
+
+
+# class Solution:
+#     # Bottom-Up (Tabulation)
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         dp = [float('inf')] * (amount + 1)
+#         # Base case; zero coins needed for amount zero.
+#         dp[0] = 0  
+
+#         # Fill the dp array for each coin.
+#         for coin in coins:
+#             for amt in range(coin, amount + 1):
+#                 # Update the minimum coins needed for the current amount.
+#                 dp[amt] = min(dp[amt], dp[amt - coin] + 1)
+
+#         # Return the result for the target amount.
+#         return dp[amount] if dp[amount] != float('inf') else -1
