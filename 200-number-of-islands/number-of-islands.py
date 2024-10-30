cff30bdb02706aa766b34166bf8023b1c7b49d9c
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2024-10-30 13:39:07 -0700

    Time: 309 ms (12.24%) | Memory: 27.8 MB (10.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,43 +1,40 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # Initialize count variable
-        count = 0
-        # Initialize a visited variable
         visited = set()
-        # iterative code to traverse through each node in the grid
+        counter = 0
+
+        def explore(r, c):
+            if r > len(grid)-1 or r < 0 or c < 0 or c > len(grid[0])-1:
+                return
+
+            if grid[r][c] == "0":
+                return
+            
+            location = str(r) + "," + str(c)
+            if location in visited:
+                return
+            
+            visited.add(location)
+            # print(visited)
+
+            # up
+            explore(r-1,c)
+            # down
+            explore(r+1,c)
+            # left
+            explore(r,c-1)
+            # right
+            explore(r,c+1)
+
+        # explore(0,0)
+
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                # explore Island when found a land
-                if grid[row][col] == "1":
-                    if self.explore(grid,row,col,visited):
-                        count += 1
-        return count
-        
-    def explore(self, grid, r, c, visited):
-        # Check Bounds, return false
-        rowBound = r < len(grid) and r >= 0
-        colBound = c < len(grid[0]) and c >= 0
-        if not rowBound or not colBound: return False
+                location = str(row) + "," + str(col)
+                if location not in visited:
+                    if grid[row][col] == "1":
+                        counter += 1
+                    explore(row,col)
+                    
 
-        # Check if Water, return false
-        if grid[r][c] == "0": return False
-
-        # Check if visited, return false
-        pos = str(r) + ',' + str(c)
-        if pos in visited: return False
-
-        # Add node to visited
-        visited.add(pos)
-
-        ## Explore neighbors
-        # Left
-        self.explore(grid,r-1,c,visited)
-        # Right
-        self.explore(grid,r+1,c,visited)
-        # Up
-        self.explore(grid,r,c-1,visited)
-        # Down
-        self.explore(grid,r,c+1,visited)
-
-        # Now that we have explored the neighbors, return True
-        return True
+        return counter
