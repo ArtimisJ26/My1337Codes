f16c9706a75430baf4e1e79e72231808fcf38c79
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2024-11-07 18:06:00 -0800

    Time: 8 ms (83.36%) | Memory: 18.1 MB (98.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -2,21 +2,19 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         if not height: return 0
 
-        size = len(height)
-        leftArr = [0] * size
-        rightArr = [0] * size        
+        # two pointers
+        l, r = 0, len(height)-1
+        leftMax, rightMax = height[l], height[r]
         res = 0
 
-        leftArr[0] = height[0]
-        rightArr[size-1] = height[size-1]
-
-        for i in range(1, size):
-            leftArr[i] = max(leftArr[i-1], height[i])
-        
-        for i in range(size - 2, -1, -1):
-            rightArr[i] = max(rightArr[i+1], height[i])
-        
-        for i in range(1, size-1):
-            res += min(leftArr[i], rightArr[i]) - height[i]
-
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax,height[r])
+                res += rightMax - height[r]
+    
         return res
