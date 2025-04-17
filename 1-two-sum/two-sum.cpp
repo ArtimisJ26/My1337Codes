4c2d0c16ab3892ab4b3da383deeb11df2911523b
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2025-04-17 12:08:01 -0700

    Time: 0 ms (100.00%) | Memory: 14.8 MB (36.20%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // Declare Hashmap
-        unordered_map<int, int> indices;
-
-        // Store index value pairs in hashmap
-        for (int i = 0; i < nums.size(); i++) {
-            indices[nums[i]] = i;
-        }
-        
+        unordered_map<int, int> m;
         // Find if (number - target) exists in the hashmap
         for (int i = 0; i < nums.size(); i++) {
-            int diff = target - nums[i];
-            if (indices.count(diff) && indices[diff] != i) {
-                return {i, indices[diff]};
+            if (m.count(target - nums[i])) {
+                return {i, m[target - nums[i]]};
             }
+            // Store index value pair in hashmap
+            m[nums[i]] = i;
         }
         return {};
     }
