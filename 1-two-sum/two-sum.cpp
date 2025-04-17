d3210703a161e2406241d05461d099612d76885d
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2025-04-17 12:04:40 -0700

    Time: 6 ms (49.84%) | Memory: 15.9 MB (6.99%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // create vector or number and index pairs
-        vector<pair<int, int>> pairs;
+        // Declare Hashmap
+        unordered_map<int, int> indices;
 
-        for (int i = 0; i < nums.size(); i++){
-            pairs.push_back({nums[i], i});
+        // Store index value pairs in hashmap
+        for (int i = 0; i < nums.size(); i++) {
+            indices[nums[i]] = i;
         }
-
-        // sort the vector
-        sort(pairs.begin(), pairs.end());
-
-        // find the pair using two pointer method
-        int l = 0, r = nums.size() - 1;
-
-        while (l < r){
-            int curr = pairs[l].first + pairs[r].first;
-            if(curr == target) {
-                return {min(pairs[l].second, pairs[r].second),
-                    max(pairs[l].second, pairs[r].second)};
-            } else if (curr > target) {
-                r--;
-            } else {
-                l++;
+        
+        // Find if (number - target) exists in the hashmap
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (indices.count(diff) && indices[diff] != i) {
+                return {i, indices[diff]};
             }
         }
         return {};
