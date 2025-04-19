8bc839c9b7c779e8a29f9176bf1af8e3f0e53a41
Juily Deshpande <77852899+ArtimisJ26@users.noreply.github.com>
2025-04-18 17:04:22 -0700

    Time: 0 ms (100.00%) | Memory: 11 MB (21.04%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -8,11 +8,12 @@ public:
             count[s[r]]++;
             maxFreq = max(maxFreq, count[s[r]]);
 
-            while ((r - l + 1) - maxFreq > k) {
+            if ((r - l + 1) - maxFreq > k) {
                 count[s[l]]--;
                 l++;
+            } else {
+                res = max(res, (r - l + 1));
             }
-            res = max(res, (r - l + 1));
         }
 
         return res;
